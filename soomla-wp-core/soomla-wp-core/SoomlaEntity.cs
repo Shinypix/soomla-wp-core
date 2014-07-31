95bcf8a9e78468875dad98f0eef0a461f405bb8a
Guillaume Passebon <guillaume.passebon@gmail.com>
2014-07-31 16:43:39 +0200

    SoomlaEntity JSONConsts

---
+++ soomla-wp-core/soomla-wp-core/SoomlaEntity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using SoomlaWpCore.data;
+
+namespace SoomlaWpCore
+{
+    public abstract class SoomlaEntity
+    {
+        public SoomlaEntity(String Name, String Description, String ID)
+        {
+            mName = Name;
+            mDescription = Description;
+            mID = ID.Trim();
+        }
+
+        public SoomlaEntity(JObject jsonObject) {
+            mName = jsonObject.Value<string>(JSONConsts.SOOM_ENTITY_NAME);
+            mDescription = jsonObject.Value<string>(JSONConsts.SOOM_ENTITY_DESCRIPTION);
+            mID = jsonObject.Value<string>(JSONConsts.SOOM_ENTITY_ID);
+        }
+
+        public JObject toJSONObject()
+        {
+            JObject jsonObject = new JObject();
+            
+            jsonObject[JSONConsts.SOOM_ENTITY_NAME] = mName;
+            jsonObject[JSONConsts.SOOM_ENTITY_DESCRIPTION] = mDescription;
+            jsonObject[JSONConsts.SOOM_ENTITY_ID] = mID;
+           
+            return jsonObject;
+        }
+
+        public bool Equal(Object o)
+        {
+            if(this == o) return true;
+            if (!(o is SoomlaEntity)) return false;
+
+            SoomlaEntity that = (SoomlaEntity)o;
+            return (mID.Equals(that.mID));
+        }
+
+        public int hashCode()
+        {
+            return mID != null ? mID.GetHashCode() : 0;
+        }
+
+        private const String TAG = "SOOMLA SoomlaEntity"; //used for Log messages
+        protected String mName;
+        protected String mDescription;
+        protected String mID;
+    }
+}
---
+++ soomla-wp-core/soomla-wp-core/data/JSONConsts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoomlaWpCore.data
+{
+    public class JSONConsts
+    {
+        /** Global **/
+        public const String SOOM_ENTITY_NAME       = "name";
+        public const String SOOM_ENTITY_DESCRIPTION = "description";
+        public const String SOOM_ENTITY_ID         = "itemId";
+        public const String SOOM_ASSOCITEMID       = "associatedItemId";
+        public const String SOOM_DESIRED_BALANCE   = "desiredBalance";
+        public const String SOOM_NAME              = "name";
+        public const String SOOM_CLASSNAME         = "className";
+
+
+        /** Reward **/
+
+        public const String SOOM_REWARDS           = "rewards";
+        public const String SOOM_REWARD_REWARDID   = "rewardId";
+        public const String SOOM_REWARD_AMOUNT     = "amount";
+        public const String SOOM_REWARD_ICONURL    = "iconUrl";
+        public const String SOOM_REWARD_REPEAT     = "repeatable";
+    }
+}
