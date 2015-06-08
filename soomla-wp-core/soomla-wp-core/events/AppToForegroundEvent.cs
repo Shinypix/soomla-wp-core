5098a2bc9f1627ee682363648ac5560c36520e36
Guillaume Passebon <guillaume.passebon@gmail.com>
2015-06-08 19:05:29 +0200

    Highway needed features

---
+++ soomla-wp-core/soomla-wp-core/events/AppToForegroundEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoomlaWpCore.events
+{
+    public class AppToForegroundEvent : SoomlaEvent
+    {
+        public AppToForegroundEvent() : this(null)
+        {
+        }
+
+        public AppToForegroundEvent(Object sender)
+            : base(sender)
+        {
+        }
+    }
+}
---
+++ soomla-wp-core/soomla-wp-core/events/RewardGivenEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoomlaWpCore.events
+{
+    public class RewardGivenEvent : SoomlaEvent
+    {
+        public string RewardId;
+
+        public RewardGivenEvent(string rewardId) : this(rewardId,null)
+        {
+        }
+
+        public RewardGivenEvent(string rewardId, Object sender)
+            : base(sender)
+        {
+            RewardId = rewardId;
+        }
+    }
+}
